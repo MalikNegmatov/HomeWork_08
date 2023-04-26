a29f3f3cdeecbc792cf84124a680f727e33471ab
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-26 11:01:12 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        task1();
+//        task2();
+//        task3();
+    }
+
+    public static void task1() {
+        System.out.println("\nЗадача №1");
+//      Пишем код для задачи 1
+    }
+}
