699ed40f19d106c5ebd74a5474e703abf13f1940
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-02 12:07:59 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,31 @@
 public class Main {
 
     public static void main(String[] args) {
-//        task1();
-//        task2();
-//        task3();
+        taskAddProblem1();
     }
 
 //    public static void task1() {
 //        System.out.println("\nЗадача №1");
-////      Пишем код для задачи 1
+//      Пишем код для задачи 1
 //    }
+
+    public static int[] generateRandomArray(int arrayLength, int numberDigits) {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[arrayLength];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt((int) Math.pow(10,numberDigits) + (int) Math.pow(10^numberDigits,numberDigits));
+        }
+        return arr;
+    }
+
+    public static void taskAddProblem1() {
+        System.out.println("\nДополнительная Задача №1");
+//      Пишем код для задачи 1
+        int[] nums = generateRandomArray(5, 5);
+        int target = 5;
+        target += nums[2];
+        System.out.println(target);
+        System.out.println(nums[0]);
+        System.out.println(nums.length);
+    }
 }
