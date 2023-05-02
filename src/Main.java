eada608dd1d2189814db2c6abba2a5039927f4ee
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-02 17:06:29 +0300

    На проверку

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@
 public class Main {
 
     public static void main(String[] args) {
-//        task1();
+        task1();
 //        task2();
 //        task3();
     }
 
-//    public static void task1() {
-//        System.out.println("\nЗадача №1");
-////      Пишем код для задачи 1
-//    }
+   public static void task1() {
+       System.out.println("\nЗадача №1");
+
+   }
 }
